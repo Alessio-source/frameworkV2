fb7ce63e2f0f8e3e4e081875a602443fa8b068a1
Alessio Pancia <alessio.pancia.ext@alten.it>
2021-12-23 12:55:40 +0100

    comando creazione model

---
+++ models/testModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace Models;
+use Classes\model;
+class testModel extends model {
+    public static $table = '';
+    public static $parameters = [
+        '1',
+        '2',
+        '3',
+    ];
+}
